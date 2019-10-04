dabb3547bea21068c161aac809fa8e10ecc37c07
TheTallPaul <paul.lantow@gmail.com>
2019-10-03 18:19:32 -0700

    Added a destination search bar.

--- lib/src/blocs/map_event.dart
+++ lib/src/blocs/map_event.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 
 @immutable
 abstract class MapEvent extends Equatable {
-  MapEvent([List props = const []]) : super(props);
+  MapEvent([List props = const []]) : super();
 }
 
-class FetchUserLocation extends MapEvent {}
+class FetchUserLocation extends MapEvent {
+  @override
+  // TODO: implement props
+  List<Object> get props => null;
+}
 
-class FetchOffices extends MapEvent {}
+class FetchOffices extends MapEvent {
+  @override
+  // TODO: implement props
+  List<Object> get props => null;
+}
