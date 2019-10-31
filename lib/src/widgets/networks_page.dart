56dd9543887073facc78c72c3facd0e3bc351bc7
TheTallPaul <paul.lantow@gmail.com>
2019-10-31 14:44:17 -0700

    Networks intro screen.

--- lib/src/widgets/networks_page.dart
+++ lib/src/widgets/networks_page.dart
@@ -8,11 +8,25 @@ class NetworksPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Networks'),
-      ),
-      drawer: NavigationDrawer(),
-      body: Text('Someday there will be a cool networks page here.'),
-    );
+        appBar: AppBar(
+          title: Text('Networks'),
+        ),
+        drawer: NavigationDrawer(),
+        body: Container(
+          child: Column(children: <Widget>[
+            Image(image: AssetImage('assets/images/networks_splash.png')),
+            Text(
+              "Networks connect you to communties of people you can trust to share rides with.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18.0),
+            ),
+            FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(fontSize: 42.0),
+                )),
+          ]),
+        ));
   }
 }
